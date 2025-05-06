25abf488f2352a39a5e428522d1ce4f6bf461cb3
Grace Birgitta Hadhinata <146738334+gracebirgita@users.noreply.github.com>
2025-05-06 12:39:01 +0700

    Update test_operation and operations

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,17 +1,24 @@
 def add(a, b):
     return a + b
+    pass
 
 
 def subtract(a, b):
     # TODO: implement this
+    return a - b
     pass
 
 
 def multiply(a, b):
     # TODO: implement this
+    return a * b
     pass
 
 
 def divide(a, b):
     # TODO: implement this (don't forget to handle division by zero)
-    pass
+    
+    if b==0:
+        return "can't divide by 0"
+    else:
+        return a / b
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -9,14 +9,22 @@ class TestCalculator(unittest.TestCase):
 
     def test_subtract(self):
         # TODO: add assertions for subtract
+        self.assertEqual(subtract(2, 2), 0)
+        self.assertEqual(subtract(3, 3), 0)
+        self.assertEqual(subtract(4, 4), 0)
         pass
 
     def test_multiply(self):
-        # TODO: add assertions for multiply
+        self.assertEqual(multiply(2, 2), 4)
+        self.assertEqual(multiply(3, 3), 9)
+        self.assertEqual(multiply(4, 4), 16)
         pass
 
     def test_divide(self):
         # TODO: add assertions for divide
+        self.assertEqual(divide(2, 2), 1)
+        self.assertEqual(divide(9, 3), 3)
+        self.assertEqual(divide(16, 0), "can't divide by 0")
         pass
 
 if __name__ == '__main__':
